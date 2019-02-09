46cab6cfe3275eb45482e6f4092417d263a27eaf
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2019-02-10 00:45:30 +0530

    Update webcam.py

--- Webcam-Face-Detect-master/webcam.py
+++ Webcam-Face-Detect-master/webcam.py
@@ -4,7 +4,7 @@ import sys
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 
 while True:
     # Capture frame-by-frame
